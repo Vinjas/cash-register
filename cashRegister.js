cfb6e5e12af4f13188886ca79ba5ff8f09e28d9f
Vinjas <79985850+Vinjas@users.noreply.github.com>
2021-03-25 14:47:15 +0100

    Update cashRegister.js

--- cashRegister.js
+++ cashRegister.js
@@ -1,75 +1,61 @@
+var denom = [
+  { name: "ONE HUNDRED", val: 100.0 },
+  { name: "TWENTY", val: 20.0 },
+  { name: "TEN", val: 10.0 },
+  { name: "FIVE", val: 5.0 },
+  { name: "ONE", val: 1.0 },
+  { name: "QUARTER", val: 0.25 },
+  { name: "DIME", val: 0.1 },
+  { name: "NICKEL", val: 0.05 },
+  { name: "PENNY", val: 0.01 }
+];
+
 function checkCashRegister(price, cash, cid) {
-    // Define Object change
-    var change = {
-      status: "",
-      change: []
-    }
-    // Calculate Total amount of Money in the cash register
-    var totalMoney = 0
-    var arrMoney = cid.slice()
-    for(let i = 0; i < arrMoney.length; i++) {
-      totalMoney += arrMoney[i][1]
-    }
-    totalMoney = Math.round(totalMoney * 100) / 100
-    
-    // Calculate remaining money after purchase
-    var remainMoney = totalMoney - cash
-  
-    // Calculate total change
-    var totalChange = cash - price
-  
-    // Create array with amount of coins and bills
-    var coins = cid.slice()
-    for(let i = 0; i < arrMoney.length; i++) {
-      switch (arrMoney[i][0]) {
-        case "PENNY":
-          coins[i][1] = Math.round(coins[i][1] /= 0.01)
-          break
-        case "NICKEL":
-          coins[i][1] = Math.round(coins[i][1] /= 0.05)
-          break
-        case "DIME":
-          coins[i][1] = Math.round(coins[i][1] /= 0.1)
-          break
-        case "QUARTER":
-          coins[i][1] = Math.round(coins[i][1] /= 0.25)
-          break
-        case "ONE":
-          coins[i][1] = Math.round(coins[i][1] /= 1)
-          break
-        case "FIVE":
-          coins[i][1] = Math.round(coins[i][1] /= 5)
-          break
-        case "TEN":
-          coins[i][1] = Math.round(coins[i][1] /= 10)
-          break
-        case "TWENTY":
-          coins[i][1] = Math.round(coins[i][1] /= 20)
-          break
-        case "ONE HUNDRED":
-          coins[i][1] = Math.round(coins[i][1] /= 100)
-          break
-      }
-    }  
-  
-  
-  
-    console.log("Total: "+totalMoney)
-    console.log("Remaining: "+remainMoney)
-    console.log("Change total: "+totalChange)
-    console.log("Coins: "+coins)
-  
-    var change = {
-      status: "",
-      change: []
-    }
-  
-  
-  
-    return change;
+  var output = { status: null, change: [] };
+  var change = cash - price;
+
+  var register = cid.reduce(
+    function(acc, curr) {
+      acc.total += curr[1];
+      acc[curr[0]] = curr[1];
+      return acc;
+    },
+    { total: 0 }
+  );
+
+  if (register.total === change) {
+    output.status = "CLOSED";
+    output.change = cid;
+    return output;
   }
-  
-  
-  
-  
-  console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+
+  if (register.total < change) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
+  }
+
+  var change_arr = denom.reduce(function(acc, curr) {
+    var value = 0;
+
+    while (register[curr.name] > 0 && change >= curr.val) {
+      change -= curr.val;
+      register[curr.name] -= curr.val;
+      value += curr.val;
+      change = Math.round(change * 100) / 100;
+    }
+
+    if (value > 0) {
+      acc.push([curr.name, value]);
+    }
+    return acc;
+  }, []);
+
+  if (change_arr.length < 1 || change > 0) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
+  }
+
+  output.status = "OPEN";
+  output.change = change_arr;
+  return output;
+}
